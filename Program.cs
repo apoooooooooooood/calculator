ab3e6378e19c64da58e8bd2a65d3bf844d4eeab1
Abdelrahman <apderahmanezat@gmail.com>
2026-08-27 12:02:05 +0300

    Add initial project files for basic calculator functionality

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter first number: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Sum: " + (num1 + num2));
+        Console.WriteLine("Subtraction: " + (num1 - num2));
+        Console.WriteLine("Multiplication: " + (num1 * num2));
+        Console.WriteLine("Division: " + (num1 / num2));
+    }
+}
